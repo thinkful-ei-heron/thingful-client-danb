5f1d5ed7a8a1a957080ecb02529131a5a7f19687
Daniel Bright <brahyt@gmail.com>
2019-11-13 21:23:03 -0800

    omg

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Button, Input } from '../Utils/Utils'
+import TokenService from '../../services/token-service'
 
 export default class LoginForm extends Component {
   static defaultProps = {
@@ -11,9 +12,8 @@ export default class LoginForm extends Component {
   handleSubmitBasicAuth = ev => {
     ev.preventDefault()
     const { user_name, password } = ev.target
-
-    console.log('login form submitted')
-    console.log({ user_name, password })
+    TokenService.saveAuthToken(
+      TokenService.makeBasicAuthToken(user_name, password))
 
     user_name.value = ''
     password.value = ''
--- src/services/thing-api-service.js
+++ src/services/thing-api-service.js
@@ -1,9 +1,11 @@
 import config from '../config'
+import TokenService from './token-service'
 
 const ThingApiService = {
   getThings() {
     return fetch(`${config.API_ENDPOINT}/things`, {
       headers: {
+        'authorization': `basic ${TokenService.getAuthToken()}`
       },
     })
       .then(res =>
@@ -15,6 +17,7 @@ const ThingApiService = {
   getThing(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}`, {
       headers: {
+        'authorization': `basic ${TokenService.getAuthToken()}`
       },
     })
       .then(res =>
@@ -26,6 +29,7 @@ const ThingApiService = {
   getThingReviews(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}/reviews`, {
       headers: {
+        'authorization': `basic ${TokenService.getAuthToken()}`
       },
     })
       .then(res =>
@@ -38,6 +42,7 @@ const ThingApiService = {
     return fetch(`${config.API_ENDPOINT}/reviews`, {
       method: 'POST',
       headers: {
+        'authorization': `basic ${TokenService.getAuthToken()}`,
         'content-type': 'application/json',
       },
       body: JSON.stringify({
